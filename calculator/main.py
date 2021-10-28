f0cd381a4901ad65d7114e0a4daf448561758014
Laughing-Bulls <asd26@njit.edu>
2021-10-28 00:00:40 -0400

    Task: Add functions on main.py to divide and take power of

--- calculator/main.py
+++ calculator/main.py
@@ -1,8 +1,10 @@
 """ This is the increment function"""
+
 class Calculator:
     """ This is the Calculator class"""
 
     result = 0
+
     def get_result(self):
         """ Get Result of Calculation"""
         return self.result
@@ -11,11 +13,26 @@ class Calculator:
         """ adds number to result"""
         self.result = self.result + value_a
         return self.result
+
     def subtract_number(self, value_a):
         """ subtract number from result"""
         self.result = self.result - value_a
         return self.result
+
     def multiply_numbers(self, value_a, value_b):
         """ multiply two numbers and store the result"""
         self.result = value_a * value_b
+        return self.result
+
+    def divide_numbers(self, value_a, value_b):
+        """ divide two numbers and store the result"""
+        try:
+            self.result = value_a / value_b
+        except ZeroDivisionError:
+            print("Division by zero is not allowed")
+        return self.result
+
+    def power_numbers(self, value_a, value_b):
+        """ compute one number to the power of another and store the result"""
+        self.result = value_a ** value_b
         return self.result
