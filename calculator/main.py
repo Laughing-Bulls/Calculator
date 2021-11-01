860502f65abd3f8e4dd59616706d45d78244ddb5
Laughing-Bulls <asd26@njit.edu>
2021-11-01 09:24:50 -0400

    Task: Change to instance method; add user input

--- calculator/main.py
+++ calculator/main.py
@@ -1,38 +1,68 @@
-""" This is the my Python calculator"""
+""" This is a Python calculator"""
+
 
 class Calculator:
     """ This is the Calculator class"""
 
-    result = 0
+    def add_numbers(self, value_a, value_b):
+        """ adds two numbers"""
+        return value_a + value_b
 
-    def get_result(self):
-        """ Get Result of Calculation"""
-        return self.result
-
-    def add_number(self, value_a):
-        """ adds number to result"""
-        self.result = self.result + value_a
-        return self.result
-
-    def subtract_number(self, value_a):
-        """ subtract number from result"""
-        self.result = self.result - value_a
-        return self.result
+    def subtract_numbers(self, value_a, value_b):
+        """ subtract numbers"""
+        return value_a - value_b
 
     def multiply_numbers(self, value_a, value_b):
         """ multiply two numbers and store the result"""
-        self.result = value_a * value_b
-        return self.result
+        return value_a * value_b
 
     def divide_numbers(self, value_a, value_b):
         """ divide two numbers and store the result"""
         try:
-            self.result = value_a / value_b
+            return value_a / value_b
         except ZeroDivisionError:
             print("Division by zero is not allowed")
-        return self.result
 
     def power_numbers(self, value_a, value_b):
         """ compute one number to the power of another and store the result"""
-        self.result = value_a ** value_b
-        return self.result
+        return value_a ** value_b
+
+    print("Welcome to the calculator!")
+    while True:
+        try:
+            value_a = float(input("Enter value a: "))
+            break
+        except ValueError:
+            print("That is not a valid number")
+
+    valid_operands = ["+", "-", "*", "/", "^"]
+    operand = input("Enter a function (+,-,*,/,^): ")
+    if operand not in valid_operands:
+        raise Exception("That is not a valid function")
+
+    while True:
+        try:
+            value_b = float(input("Enter value b: "))
+            break
+        except ValueError:
+            print("That is not a valid number")
+
+    if operand == "+":
+        print(str(value_a) + " + " + str(value_b) + " = " + str(add_numbers(value_a, value_b)))
+
+    if operand == "-":
+        print(str(value_a) + " - " + str(value_b) + " = " + str(subtract_numbers(value_a, value_b)))
+
+    if operand == "*":
+        print(str(value_a) + " * " + str(value_b) + " = " + str(multiply_numbers(value_a, value_b)))
+
+    if operand == "/":
+        if value_b == "0":
+            raise Exception("Division by zero is not allowed")
+        else:
+            print(str(value_a) + " / " + str(value_b) + " = " + str(divide_numbers(value_a, value_b)))
+
+    if operand == "^":
+        print(str(value_a) + "^" + str(value_b) + " = " + str(power_numbers(value_a, value_b)))
+
+    print("That's all, Folks!")
