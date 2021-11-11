587f51eb96f7d08d17b50e2098005949397e1f87
Laughing-Bulls <asd26@njit.edu>
2021-11-11 18:25:58 -0500

    Task: Modify README.md. Finalize user test main.py.

--- calc/main.py
+++ calc/main.py
@@ -1,19 +1,10 @@
-""" This is a script to run the Python calculator"""
+""" This is a script to run a user test of the Python calculator"""
 
 from calc.calculator import Calculator
-from calc.history.calculations import Calculations
 
 
 def main():
     """ User test of the calculator program"""
-
-    Calculations.add_to_history(2)
-    print(Calculations.get_history_result())
-
-    Calculator.add_numbers(2.0, 3.0, 4.0)
-    print(Calculations.get_history_result())
-
-    Calculations.clear_history()
 
     print("Welcome to the calculator!")
     while True:
@@ -35,26 +26,28 @@ def main():
         except ValueError:
             print("That is not a valid number")
 
+    inputs = (value_a, value_b)
+
     if operand == "+":
-        answer = Calculator.add_numbers(value_a, value_b)
+        answer = Calculator.add_numbers(inputs)
         print(str(value_a) + " + " + str(value_b) + " = " + str(answer))
 
     if operand == "-":
-        answer = Calculator.subtract_numbers(value_a, value_b)
+        answer = Calculator.subtract_numbers(inputs)
         print(str(value_a) + " - " + str(value_b) + " = " + str(answer))
 
     if operand == "*":
-        answer = Calculator.multiply_numbers(value_a, value_b)
+        answer = Calculator.multiply_numbers(inputs)
         print(str(value_a) + " * " + str(value_b) + " = " + str(answer))
 
     if operand == "/":
         if value_b == 0:
             raise Exception("Division by zero is not allowed")
-        answer = Calculator.divide_numbers(value_a, value_b)
+        answer = Calculator.divide_numbers(inputs)
         print(str(value_a) + " / " + str(value_b) + " = " + str(answer))
 
     if operand == "^":
-        answer = Calculator.power_numbers(value_a, value_b)
+        answer = Calculator.power_numbers(inputs)
         print(str(value_a) + "^" + str(value_b) + " = " + str(answer))
 
     print("That's all, Folks!")
