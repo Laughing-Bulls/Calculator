ba800540d937fb8339e5a63149bc6b6083981d7a
Laughing-Bulls <asd26@njit.edu>
2021-12-09 12:31:26 -0500

    Task: move calculator_controller.py calculation to fileops.py

--- app/controllers/calculator_controller.py
+++ app/controllers/calculator_controller.py
@@ -1,5 +1,7 @@
 from app.controllers.controller import ControllerBase
-from calc.calculator import Calculator
+from filehandling.fileops import Fileops
+# from calc.calculator import Calculator
+
 from flask import Flask, render_template, request, flash, redirect, url_for
 
 
@@ -14,12 +16,15 @@ class CalculatorController(ControllerBase):
         operation = request.form['operation']
         is_no_error = True
         error = ""
+
+        # make sure first two input values contain numbers or return error and reload page
         try:
             value1 = float(value1)
             value2 = float(value2)
         except ValueError:
             is_no_error = False
             error = 'ERROR: You must enter a numerical value for value 1 and value 2'
+
         if is_no_error:
             # make the tuple
             try:
@@ -28,10 +33,11 @@ class CalculatorController(ControllerBase):
             except ValueError:
                 value3 = ""
                 input_tuple = (value1, value2)
-            # call the correct operation
-            getattr(Calculator, operation)(input_tuple)
-            # get calculation result from History
-            result = str(Calculator.get_last_result_value())
+
+            # calculate result and add to history file
+            result = str(Fileops.add_calculation_to_history_file(input_tuple, operation))
+
+            # note any computation errors with flash messages
             if operation == "division" and result == "None":
                 is_no_error = False
                 error = 'ERROR: Division by zero is not allowed'
@@ -40,7 +46,9 @@ class CalculatorController(ControllerBase):
                 error = 'ERROR: Inputs are too large for exponent calculation'
             if is_no_error:
                 flash("Calculation Successful!")
-            return render_template('result.html', value1=value1, value2=value2, value3=value3, operation=operation, result=result, error=error)
+
+            return render_template('result.html', value1=value1, value2=value2, value3=value3, operation=operation,
+                                   result=result, error=error)
         else:
             return render_template('calculator.html', error=error)
 
